604bb680c46a9c010636a2d0bb1c1c826554a5e6
Damian Kolaska <damian.kolaska99@gmail.com>
2023-06-18 12:46:28 +0200

    Add api gateway object versioning

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,15 +1,23 @@
+locals {
+  version = "v1.0.0"
+}
+
 resource "google_api_gateway_api" "postspot_api" {
   project = var.project_id
   provider = google-beta
-  api_id = "postspot-api-${var.environment}"
+  api_id = "postspot-api-${var.environment}-${local.version}"
   display_name = "PostSpot API"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "google_api_gateway_api_config" "postspot_api_config" {
   project = var.project_id
   provider = google-beta
   api = google_api_gateway_api.postspot_api.api_id
-  api_config_id = "postspot-api-config-${var.environment}"
+  api_config_id = "postspot-api-config-${var.environment}-${local.version}"
 
   openapi_documents {
     document {
@@ -29,7 +37,11 @@ resource "google_api_gateway_gateway" "postspot_api_gateway" {
   region = var.api_gateway_region
   provider = google-beta
   api_config = google_api_gateway_api_config.postspot_api_config.id
-  gateway_id = "postspot-api-gateway-eu-${var.environment}"
+  gateway_id = "postspot-api-gateway-eu-${var.environment}-${local.version}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   depends_on = [google_api_gateway_api_config.postspot_api_config]
 }
