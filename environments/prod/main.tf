f0c58ae6a48404038f0fbc02eae256027d6f3eac
Damian Kolaska <damian.kolaska99@gmail.com>
2023-06-20 23:27:02 +0200

    Fix

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -23,7 +23,6 @@ resource "google_api_gateway_api_config" "postspot_api_config" {
         templatefile(
           "../../openapi.yaml",
           {
-            version                  = local.version,
             user_service_url         = var.user_service_url,
             post_service_url         = var.post_service_url,
             notification_service_url = var.notification_service_url,
