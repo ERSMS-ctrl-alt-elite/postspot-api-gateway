da0ab3c235a521663fce195517f43c231f9a48c0
Damian Kolaska <damian.kolaska99@gmail.com>
2023-06-21 20:07:03 +0200

    Fix.

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -2,7 +2,6 @@ module "postspot_api_gateway" {
   source = "./modules/api_gateway"
 
   project_id               = var.project_id
-  region                   = var.region
   api_gateway_region       = var.api_gateway_region
   environment              = var.environment
   user_service_url         = var.user_service_url
