bd6db5fb9421cebff45e7bb18b166e5047261e5a
sweklej <sweklej.bartek@gmail.com>
2023-06-21 20:03:59 +0200

    even with prod

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,8 +1,3 @@
-locals {
-  version = "1.0.9"
-  suffix  = "${var.environment}-v${replace(local.version, ".", "-")}"
-}
-
 /* -------------------------------------------------------------------------- */
 /*                                 API Gateway                                */
 /* -------------------------------------------------------------------------- */
@@ -10,19 +5,16 @@ locals {
 resource "google_api_gateway_api" "postspot_api" {
   project      = var.project_id
   provider     = google-beta
-  api_id       = "postspot-api-${local.suffix}"
+  api_id       = "postspot-api-${var.environment}"
   display_name = "PostSpot API"
 
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "google_api_gateway_api_config" "postspot_api_config" {
   project       = var.project_id
   provider      = google-beta
   api           = google_api_gateway_api.postspot_api.api_id
-  api_config_id = "postspot-api-config-${local.suffix}"
+  api_config_id = "postspot-api-config-${var.environment}-${var.commit_sha}"
 
   openapi_documents {
     document {
@@ -31,7 +23,6 @@ resource "google_api_gateway_api_config" "postspot_api_config" {
         templatefile(
           "../../openapi.yaml",
           {
-            version                  = local.version,
             user_service_url         = var.user_service_url,
             post_service_url         = var.post_service_url,
             notification_service_url = var.notification_service_url,
@@ -40,11 +31,10 @@ resource "google_api_gateway_api_config" "postspot_api_config" {
       )
     }
   }
+
   lifecycle {
     create_before_destroy = true
   }
-
-  depends_on = [google_api_gateway_api.postspot_api]
 }
 
 resource "google_api_gateway_gateway" "postspot_api_gateway" {
@@ -52,11 +42,65 @@ resource "google_api_gateway_gateway" "postspot_api_gateway" {
   region     = var.api_gateway_region
   provider   = google-beta
   api_config = google_api_gateway_api_config.postspot_api_config.id
-  gateway_id = "postspot-api-gateway-eu-${local.suffix}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  gateway_id = "postspot-api-gateway-eu-${var.environment}"
 
   depends_on = [google_api_gateway_api_config.postspot_api_config]
+}
+
+resource "google_compute_global_network_endpoint_group" "postspot_neg" {
+  name                  = "postspot-neg"
+  network_endpoint_type = "INTERNET_FQDN_PORT"
+
+  depends_on = [google_api_gateway_gateway.postspot_api_gateway]
+}
+
+resource "google_compute_global_network_endpoint" "postspot_default_endpoint" {
+  global_network_endpoint_group = google_compute_global_network_endpoint_group.postspot_neg.name
+  fqdn       = google_api_gateway_gateway.postspot_api_gateway.default_hostname
+  port       = 443
+
+  depends_on = [ google_compute_global_network_endpoint_group.postspot_neg ]
+}
+
+resource "google_compute_backend_service" "postspot_backend_service" {
+  name                    = "api-gateway-backend-service"
+  protocol                = "HTTPS"
+  custom_request_headers  = ["host: ${google_compute_global_network_endpoint.postspot_default_endpoint.fqdn}"]
+
+  backend {
+    group = google_compute_global_network_endpoint_group.postspot_neg.id
+  }
+
+  depends_on = [ google_compute_global_network_endpoint.postspot_default_endpoint ]
+}
+
+resource "google_compute_url_map" "postspot_url_map" {
+  name = "api-gateway-url-map"
+  default_service = google_compute_backend_service.postspot_backend_service.id
+
+  depends_on = [ google_compute_backend_service.postspot_backend_service ]
+}
+
+resource "google_compute_managed_ssl_certificate" "postspot_ssl_cert" {
+  name = "postspot-cert"
+  
+  managed {
+    domains = [var.domain]
+  }
+}
+
+resource "google_compute_target_https_proxy" "postspot_target_https_proxy" {
+  name             = "api-gateway-https-proxy"
+  url_map          = google_compute_url_map.postspot_url_map.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.postspot_ssl_cert.id]
+
+  depends_on = [ google_compute_url_map.postspot_url_map ]
+}
+
+resource "google_compute_global_forwarding_rule" "postspot_forwarding_rules" {
+  name       = "my-fw"
+  target     = google_compute_target_https_proxy.postspot_target_https_proxy.id
+  port_range = 443
+
+  depends_on = [ google_compute_target_https_proxy.postspot_target_https_proxy ]
 }
