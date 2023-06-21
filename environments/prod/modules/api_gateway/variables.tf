2ea6fa9c39b4c41bcc2f8151b2a18bdc4988e246
Damian Kolaska <damian.kolaska99@gmail.com>
2023-06-21 20:01:45 +0200

    Refactor Terraform configuration into modules.

---
+++ environments/prod/modules/api_gateway/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  type = string
+}
+
+variable "api_gateway_region" {
+  type    = string
+  default = "europe-west2"
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "user_service_url" {
+  type = string
+}
+
+variable "post_service_url" {
+  type = string
+}
+
+variable "notification_service_url" {
+  type = string
+}
+
+variable "commit_sha" {
+  type = string
+}
---
+++ environments/prod/modules/load_balancer/variables.tf
@@ -0,0 +1,4 @@
+variable "domain" {
+  type = string
+  default = "postspot.gro4t.dev"
+}
